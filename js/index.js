a3fddb332c4b438aa824d08dc2b6bc70e8125560
Ellukitas-123 <67011864+Ellukitas-123@users.noreply.github.com>
2023-12-12 16:11:49 +0000

    Added display

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
+const override = $("#override");
 const foodContainer = $("#food-container");
 const foodTemplate = `<div class="food">
 <img
   class="display"
   src="#IMAGE#"
+  id="#NAME#"
 />
 <div class="info">
   <h4>#NAME#</h4>
@@ -21,9 +23,40 @@ async function displayMenu() {
   const menu = await (await fetch("./menu.json")).json();
 
   menu.forEach((food) => {
-    const foodElement = foodTemplate
+    const foodHTML = foodTemplate
       .replace("#IMAGE#", food.image)
-      .replace("#NAME#", food.name);
+      .replace(/#NAME#/g, food.name);
+    const foodElement = $(foodHTML);
+
+    foodElement.on("click", () => {
+      const element = $(foodHTML);
+
+      element.css({
+        position: "fixed",
+        top: foodElement.offset().top,
+        left: foodElement.offset().left,
+        zIndex: 5,
+        width: foodElement.innerWidth(),
+        height: foodElement.innerHeight(),
+      });
+      override.append(element);
+      element.animate(
+        {
+          top: "1rem",
+          left: "1rem",
+          width: window.innerWidth - 32,
+          height: window.innerHeight - 32,
+        },
+        () => {
+          element.css({ flexDirection: "column" });
+        }
+      );
+      element.find(".display").animate({
+        width: "100%",
+        height: "25%",
+      });
+    });
+
     foodContainer.append(foodElement);
   });
 }
